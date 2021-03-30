72703cde88783c22c750d1362898bdd01ece53f1
Alok Choudhary <alok.c83@gmail.com>
2021-03-29 21:42:34 -0400

    ubuntu on oracle cloud

---
+++ oracle-ubuntu/1_setup.sh
@@ -0,0 +1,19 @@
+echo “y” |  sudo apt-get install build-essential
+sudo apt-get update
+
+######## Installing Homebrew ############
+echo "Installing Homebrew"
+echo "🍺🍺🍺🍺🍺🍺🍺🍺🍺🍺🍺🍺🍺🍺🍺🍺🍺🍺🍺🍺🍺🍺🍺🍺🍺🍺🍺🍺🍺🍺🍺🍺" 
+cat /dev/null
+ | sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
+
+echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> /home/ubuntu/.profile
+
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+
+brew help
+
+################# Installing Github CLI #####################
+echo "Installing github CLI"
+echo "🐙🐙🐙🐙🐙🐙🐙🐙🐙🐙🐙🐙🐙🐙🐙🐙🐙🐙🐙🐙🐙🐙🐙🐙🐙🐙🐙🐙🐙🐙🐙🐙"
+brew install gh 
